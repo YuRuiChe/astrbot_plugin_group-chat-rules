840c2ce167965f31c46b4d3d21ccaaf1961c5f77
语芮澈 <WrcJimmy@outlook.com>
2026-03-21 11:27:43 +0800

    修修补补

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from astrbot.api import logger
 from astrbot.core import AstrBotConfig
 
 # @register 装饰器用于注册插件，参数依次为：插件名、作者、描述、版本、仓库地址
-@register("astrbot_plugin_group-chat-rules", "语芮澈", "可以判断群规是否适合当前场景", "v5", "https://github.com/YuRuiChe/astrbot_plugin_group-chat-rules")
+@register("astrbot_plugin_group-chat-rules", "语芮澈", "可以判断群规是否适合当前场景", "v6", "https://github.com/YuRuiChe/astrbot_plugin_group-chat-rules")
 class MyPlugin(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
@@ -26,7 +26,7 @@ class MyPlugin(Star):
 
     # @filter.command 装饰器定义一个指令
     @filter.command("群规判断")
-    async def query_regulations (self, event: AstrMessageEvent, prompt: str):
+    async def query_regulations(self, event: AstrMessageEvent, prompt: str):
         '''判断此言论是否符合群规'''  # 这里的注释会被解析为指令描述，建议填写
         user_name = event.get_sender_name() # 获取发送者的名字
         logger.info(f"用户 {user_name} 发送了群规请求") # 使用AstrBot的日志接口
@@ -52,7 +52,7 @@ class MyPlugin(Star):
             yield event.plain_result(f"❌ 调用 LLM 时出错: {str(e)}")
 
     @filter.event_message_type(filter.EventMessageType.ALL)
-    async def on_message(self, event: AstrMessageEvent, prompt):
+    async def on_message(self, event: AstrMessageEvent):
         """群规判断"""
         # 检查是否启用
         if self.config.get("open_review", False):
@@ -69,20 +69,17 @@ class MyPlugin(Star):
             return
         # 获取消息内容
         message = event.message_str
-        # 获取发送者的名字
-        user_name = event.get_sender_name()
         try:
             # 获取配置文件选择的LLM提供商ID
             provider_id = self.llm_provide
             if not provider_id:
                 yield event.plain_result("❌无法获取 LLM 提供商，请先在 WebUI 中配置 LLM")
                 return
-            logger.info(f"使用提供商: {provider_id}, 用户问题: {prompt}")
             # 调用 LLM 生成回答
             llm_response = await self.context.llm_generate(
                 chat_provider_id=provider_id,
-                prompt=prompt,
-                system_prompt=f"群规如下“{self.is_regulations}” 判断此言论“{prompt}”是否符合群规，如果违反，请输出“Y”这个字母；如果没有违反，那就输出“N”这个字母，坚决不要有多余的输出。"
+                prompt=message,
+                system_prompt=f"群规如下“{self.is_regulations}” 判断此言论“{message}”是否符合群规，如果违反，请输出“Y”这个字母；如果没有违反，那就输出“N”这个字母，坚决不要有多余的输出。"
             )
             # 获取回复内容
             reply = llm_response.completion_text
