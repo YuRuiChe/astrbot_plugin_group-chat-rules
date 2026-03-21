c5d2e283ca0fa90314d4480bdef14749345f059a
语芮澈 <WrcJimmy@outlook.com>
2026-03-21 09:51:31 +0800

    修修补补

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from astrbot.api import logger
 from astrbot.core import AstrBotConfig
 
 # @register 装饰器用于注册插件，参数依次为：插件名、作者、描述、版本、仓库地址
-@register("astrbot_plugin_group-chat-rules", "语芮澈", "可以判断群规是否适合当前场景", "v1.0.1", "https://github.com/YuRuiChe/astrbot_plugin_group-chat-rules")
+@register("astrbot_plugin_group-chat-rules", "语芮澈", "可以判断群规是否适合当前场景", "v1.0.2", "https://github.com/YuRuiChe/astrbot_plugin_group-chat-rules")
 class MyPlugin(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
         # 这里可以初始化插件的配置或资源
+        self.llm_provide = config.get("llm_provide", "不选择提供商")
         self.is_regulations = config.get("is_regulations", "我是群规")
+
+        if self.llm_provide:
+            logger.info(f"插件使用提供商: {self.llm_provide}")
+        else:
+            logger.info("未选择提供商，请用户在配置中选择")
+
 
 
     # @filter.command 装饰器定义一个指令
@@ -17,17 +24,15 @@ class MyPlugin(Star):
     async def query_regulations (self, event: AstrMessageEvent, prompt: str):
         '''判断此言论是否符合群规'''  # 这里的注释会被解析为指令描述，建议填写
         user_name = event.get_sender_name() # 获取发送者的名字
-        logger.info(f"用户 {user_name} 发送了群规请求") # 使用 AstrBot 的日志接口
+        logger.info(f"用户 {user_name} 发送了群规请求") # 使用AstrBot的日志接口
         try:
-            # 1. 获取当前会话的唯一标识
-            umo = event.unified_msg_origin
-            # 2. 获取当前会话使用的 LLM 提供商 ID
-            provider_id = await self.context.get_current_chat_provider_id(umo=umo)
+            # 获取配置文件选择的LLM提供商ID
+            provider_id = self.llm_provide
             if not provider_id:
-                yield event.plain_result("❌ 无法获取 LLM 提供商，请先在 WebUI 中配置 LLM")
+                yield event.plain_result("❌无法获取 LLM 提供商，请先在 WebUI 中配置 LLM")
                 return
             logger.info(f"使用提供商: {provider_id}, 用户问题: {prompt}")
-            # 3. 调用 LLM 生成回答
+            # 调用 LLM 生成回答
             llm_response = await self.context.llm_generate(
                 chat_provider_id=provider_id,
                 prompt=prompt,
